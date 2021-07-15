bee64908fec4c2e139dffd928f7007ad1b090c29
John Laubscher <jlmemphiscommanders@gmail.com>
2021-07-14 19:52:49 -0500

    checkschemeid  middleware working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,5 +1,17 @@
-function find() { // EXERCISE A
-  /*
+const db = require("../../data/db-config");
+
+async function find() {
+  // EXERCISE A
+
+  const schemes = await db("schemes as sc").leftJoin("steps as st", "sc.scheme_id", "st.scheme_id").groupBy("sc.scheme_id").orderBy("sc.scheme_id");
+  console.log("inside the find model, here's the schemes:", schemes);
+
+  const { scheme_id, scheme_name, number_of_steps } = schemes;
+  const formattedSchemes = schemes.reduce((acc, { scheme_id, scheme_name, number_of_steps }) => {}, []);
+  console.log("this is formatted schemes:", formattedSchemes);
+  return formattedSchemes;
+}
+/*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
 
@@ -15,9 +27,9 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
-}
 
-function findById(scheme_id) { // EXERCISE B
+function findById(scheme_id) {
+  // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -85,7 +97,8 @@ function findById(scheme_id) { // EXERCISE B
   */
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+function findSteps(scheme_id) {
+  // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -108,13 +121,15 @@ function findSteps(scheme_id) { // EXERCISE C
   */
 }
 
-function add(scheme) { // EXERCISE D
+function add(scheme) {
+  // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+function addStep(scheme_id, step) {
+  // EXERCISE E
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
@@ -128,4 +143,4 @@ module.exports = {
   findSteps,
   add,
   addStep,
-}
+};
